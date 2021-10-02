9d2e1240af557d13780b052b97f5b70bb8db5855
Haruki Miyazaki <83580034+HarukiMiya@users.noreply.github.com>
2021-10-03 02:50:13 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -4,6 +4,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+//hello from the other side
+
 const Home: NextPage = () => {
   // https://test-flight-data.herokuapp.com/flights?date=2020-01-01
   const [flights, setFlights] = React.useState<any[]>([]);
